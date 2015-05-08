cd44a9572191b6b2f0464758d605035425c874f6
App Academy Student <student@appacademy.io>
2015-05-08 11:04:17 -0700

    Write my_attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,12 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |var|
+      define_method(var) do
+        instance_variable_get("@#{var.to_s}")
+      end
+      define_method("#{var}=") do |value|
+        instance_variable_set("@#{var.to_s}", value)
+      end
+    end
   end
 end
